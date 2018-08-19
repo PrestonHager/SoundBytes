9a77d60ccb4028d46c7fe0c262d5c5177cb19f3a
Preston Hager <preston@hagerfamily.com>
2018-08-19 13:20:24 -0700

    Added email verification.

--- handler.py
+++ handler.py
@@ -31,11 +31,12 @@ class App:
             import json
             import os
         elif function == "create_account":
-            global base64, boto3, hashlib, json, os
+            global base64, boto3, hashlib, json, re, os
             import base64
             import boto3
             import hashlib
             import json
+            import re
             import os
         elif function == "refresh_client":
             global boto3, datetime, json, random
@@ -270,10 +271,23 @@ class App:
         response = self.create_response(body, 200)
         return response
 
+    def verify_json(self, data, params):
+        for param in params:
+            if param not in data:
+                return param
+        return False
+
     def create_account(self, event, context):
         self._import("create_account")
         self.init_users()
         data = json.loads(event["body"])
+        verify = self.verify_json(data, ["username", "password", "email"])
+        if verify:
+            body = {
+                "err": "The '{param}' parameter was not found in the supplied JSON.".format(param=verify),
+                "cod": 11
+            }
+            return self.create_response(body, 400)
         try:
             self.users.get_item(Key = {"Username": data["username"]})["Item"]
             body = {
@@ -282,8 +296,16 @@ class App:
             }
             return self.create_response(body, 400)
         except:
+            # validate the email string.
+            # NOTE: the is a very basic regex and is not the complete offical.
+            if not re.match(r"[a-zA-Z0-9\.-]+@[a-zA-Z0-9-]\.[a-zA-Z0-9]", data["email"]):
+                body = {
+                    "err": "Invalid email address.",
+                    "cod": 10
+                }
+                return self.create_response(body, 400)
             password_hash = hashlib.blake2b(bytes(data["password"], "utf-8"), salt=bytes(os.getenv('SALT', "123456abcdef"), "utf-8")).digest()
-            user = {"Username": data["username"], "Password": base64.b64encode(password_hash).decode("utf-8")}
+            user = {"Username": data["username"], "Password": base64.b64encode(password_hash).decode("utf-8"), "Email": data["email"], "Verified": false}
             self.users.put_item(Item = user)
             body = {
                 "cod": 101
