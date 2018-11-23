afc9c1f1fd898b7f66041d15635f21878d49d010
Preston Hager <preston@hagerfamily.com>
2018-11-23 13:40:50 -0800

    Renamed a folder.

--- handler/__init__.py
+++
@@ -1,9 +0,0 @@
-import .auth
-import .create_account
-import .databases
-import .get_bites
-import .intrests
-import .refresh_client
-import .upload
-
-__all__ = ["auth", "create_account", "databases", "get_bites", "intrests", "refresh_client", "upload"]
